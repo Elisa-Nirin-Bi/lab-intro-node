ef919f8e89b65c0fbb28634661749dda53e11274
Elisa Barontini <nirinproductions@gmail.com>
2021-08-12 17:32:24 +0100

    completed

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
+  
+  add(item) {
+    
+    this.items.push(item);
+    this.items.sort((a, b) => a - b);
+    this.length++;
+   
+  }
 
-  add(item) {}
+  get(pos) {
+ 
+    if(pos >= this.length){
+      throw new Error('OutOfBounds');
+    }else{
+      this.items[pos];
+    }
+  }
 
-  get(pos) {}
+    
+  max() {
+    
+    if(!this.length){
+      throw new Error('OutOfBounds');
+    }else{
+      return this.items[this.length -1];
+    }
+  }
 
-  max() {}
+  min() {
+    
+    if (!this.length) throw new Error('EmptySortedList');
+      return this.items[0]
+    
+    
+  }
 
-  min() {}
+  sum() {
+    if (this.length == 0) {
+      return 0;
+    }
+    return this.items.reduce((a,b) => a+b)
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    if (!this.length) throw new Error('EmptySortedList');
+    return this.items.reduce((a,b) => a+b)/ this.items.length;
+    
+  }
 }
 
 module.exports = SortedList;
